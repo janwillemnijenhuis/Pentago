bf6c73febd8854ab0874496177ed4fe6646de999
janwillem.nijenhuis <j.w.nijenhuis@student.utwente.nl>
2021-12-30 11:47:37 +0100

    create board and marble stub implementation

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,196 @@
 public class Board {
+
+    private Marble[][] fields;
+    private Marble[][] quadrant;
+    private final int DIM = 6;
+
+    /**
+     * initialize empty board
+     */
+    public Board() {
+
+    }
+
+    /**
+     * returns copy of the board
+     * @return the deepcopy
+     */
+    public Board deepCopy() {
+        return null;
+    }
+
+    /**
+     * transforms row,col pair to index from 0..35
+     * @param row index
+     * @param col index
+     * @return index on board
+     */
+    public int getIndex(int row, int col) {
+        return 0;
+    }
+
+    /**
+     * checks if index if valid 0..35
+     * @param index
+     * @return true if valid, false otherwise
+     */
+    public boolean isField(int index) {
+        return false;
+    }
+
+    /**
+     * checks if index is valid row,col pair
+     * @param row index
+     * @param col index
+     * @return true if valid, false if not
+     */
+    public boolean isField(int row, int col) {
+        return false;
+    }
+
+    /**
+     * returns the marble currently in the field by index
+     * @param index
+     * @return the marble in the field
+     */
+    public Marble getField(int index) {
+        return null;
+    }
+
+    /**
+     * returns the marble currently in the field by row,col pair
+     * @param row index
+     * @param col index
+     * @return the marble in the field
+     */
+    public Marble getField(int row, int col) {
+        return null;
+    }
+
+    /**
+     * check if field is empty
+     * @param index
+     * @return true if empty, false if not
+     */
+    public boolean isEmpty(int index) {
+        return false;
+    }
+
+    /**
+     * check if field is empty
+     * @param row index
+     * @param col index
+     * @return true if empty, false if not
+     */
+    public boolean isEmpty(int row, int col) {
+        return false;
+    }
+
+    /**
+     * check if board is full
+     * @return true if full, false otherwise
+     */
+    public boolean isFull() {
+        return false;
+    }
+
+    /**
+     * check if game is terminated
+     * board is full or board has winner
+     * @return true if game over, false otherwise
+     */
+    public boolean gameOver() {
+        return false;
+    }
+
+    /**
+     * check if marble has a full row (i.e. 5 marbles)
+     * @param marble the marble to check for
+     * @return true if full row, false otherwise
+     */
+    public boolean hasRow(Marble marble) {
+        return false;
+    }
+
+    /**
+     * check if marble has a full col (i.e. 5 marbles)
+     * @param marble the marble to check for
+     * @return true if full col, false otherwise
+     */
+    public boolean hasCol(Marble marble) {
+        return false;
+    }
+
+    /**
+     * check if marble has a full diagonal (i.e. 5 marbles)
+     * @param marble the marble to check for
+     * @return true if full diagonal, false otherwise
+     */
+    public boolean hasDiagonal(Marble marble) {
+        return false;
+    }
+
+    /**
+     * check if marble has won
+     * @param marble to check for
+     * @return true if has won, false otherwise
+     */
+    public boolean isWinner(Marble marble) {
+        return false;
+    }
+
+    /**
+     * check if the board is at a draw
+     * 1. both players win
+     * 2. board is full
+     * @return true if draw, false otherwise
+     */
+    public boolean isDraw() {
+        return false;
+    }
+
+    /**
+     * check if board has a winner
+     * @return true if someone has won, false otherwise
+     */
+    public boolean hasWinner() {
+        return false;
+    }
+
+    /**
+     * prints the string representation of the board
+     * it shows the numbering of the fields
+     * we make one board which represents both the numbers and placed
+     * marbles at the same time
+     * @return String representation of the current board
+     */
+    public String toString() {
+        return null;
+    }
+
+    /**
+     * resets the board to initial state
+     */
+    public void reset() {
+
+    }
+
+    /**
+     * sets field at this integer to this marble
+     * @param index of the field
+     * @param marble the marble to set the field to
+     */
+    public void setField(int index, Marble marble) {
+
+    }
+
+    /**
+     * sets field at this row,col pair to this marble
+     * @param row index
+     * @param col index
+     * @param marble the marble to set the field to
+     */
+    public void setField(int row, int col, Marble marble) {
+
+    }
 }
--- src/Marble.java
+++ src/Marble.java
@@ -1,3 +1,15 @@
 public enum Marble {
+    EMPTY,
+    BLACK,
+    WHITE;
 
+    /**
+     * checks if marble is equal to the other marble
+     * @param marble marble to check
+     * @param other marble to compare to
+     * @return true if equal, false otherwise
+     */
+    public boolean isEqual(Marble marble, Marble other) {
+        return false;
+    }
 }
